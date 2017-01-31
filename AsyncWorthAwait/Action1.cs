0827dff9d06883cc28b12f639f3b875542aa75e3
Salman Fatmi <salman.fatmi@bglgroup.co.uk>
2017-01-31 12:25:10 +0000

    Add project files.

---
+++ AsyncWorthAwait/Action1.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncWorthAwait
+{
+    internal class Action1
+    {
+        internal static Task<string> Execute()
+        {  
+            return Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                return "Action 1 completed";
+            });
+        }
+    }
+}
+
---
+++ AsyncWorthAwait/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace AsyncWorthAwait
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void action1_Click(object sender, RoutedEventArgs e)
+        {
+            label1.Content = "Action 1 started...";
+            var actionResult = await Action1.Execute();
+            label1.Content = actionResult;
+        }
+    }
+}
+ 
